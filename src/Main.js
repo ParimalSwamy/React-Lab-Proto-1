4827c86885ea37addf2fa6d8c5a767360109c68f
ParimalSwamy <goonaparimalswamy@gmail.com>
2021-10-13 01:34:13 +0530

    First panel added

---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import DateTimePanel from './DateTimePanel.js'
+import './Styles/Main.css'
+
+class Main extends Component{
+    render(){
+        return(<div>
+             <DateTimePanel/>
+        </div>
+           
+        );
+    }
+}
+
+export default Main
