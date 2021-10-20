00213892556aaf8a8ced990d459bfc3462e0ab95
ParimalSwamy <goonaparimalswamy@gmail.com>
2021-10-21 01:59:37 +0530

    Added delete/done/edit features for to-do

--- src/js/ListElement.js
+++ src/js/ListElement.js
@@ -2,22 +2,91 @@ import React from 'react'
 import { Component } from 'react';
 
 class ListElement extends Component{
+
+    constructor(props){
+        super(props);
+        this.deleteTodo = this.deleteTodo.bind(this);
+        this.tickTodo = this.tickTodo.bind(this);
+        this.submitEdit = this.submitEdit.bind(this);
+    }
+
+    deleteTodo(id) {
+        const updatedTodos = [...this.props.todos].filter((todo)=> todo.id !== id)
+        this.props.setTodos(updatedTodos)
+    }
+
+    tickTodo(id){
+        const updatedTodos = [...this.props.todos].filter((todo)=>{
+            if(todo.id === id){
+                todo.completed = !todo.completed;
+            }
+            return todo
+        }
+        )
+        this.props.setTodos(updatedTodos)
+    }
+
+    submitEdit(id){
+        const updatedTodos = [...this.props.todos].map((todo) => {
+            if (todo.id === id) {
+              todo.text = this.props.editingText;
+            }
+            return todo;
+          });
+          this.props.setTodos(updatedTodos);
+          this.props.setTodoEditing(null);
+          this.props.setEditingText("")  
+    }
+
+  
+
+
     render(){
         return(
             <div className="list-e">
                 <div className="list-e-text">
-                    <p className="list-e-text-todo">{this.props.todo}</p>
+                   
+                        {this.props.todo.id === this.props.todoEditing ?
+                        (<input type="text" className="list-e-text-editing" value={this.props.editingText} className="list-e-text-editing" onChange={(e)=>this.props.setEditingText(e.target.value)} />)
+                        :
+                        (<p className="list-e-text-todo">{this.props.todo.text}</p>)
+                        }
+                    
                     <p className="list-e-text-time">{this.props.time}</p>
                 </div>
+
                 <div className="list-e-btn">
-                    <i className="list-e-btn-edit"></i>
-                    <i className="list-e-btn-done"></i>
-                    <i className="list-e-btn-del"></i>
+                    {this.props.todo.id === this.props.todoEditing ? 
+                    (<i onClick={()=>this.submitEdit(this.props.todo.id)} className="list-e-btn-submit"></i>) 
+                    : 
+                    (<i onClick={()=>this.props.setTodoEditing(this.props.todo.id)} className="list-e-btn-edit"></i>)
+                }
+                    {this.props.todo.completed ?
+                (<i 
+                    id="completed"
+                    checked={this.props.todo.completed}
+                    onClick={()=>this.tickTodo(this.props.todo.id)} 
+                    className="list-e-btn-done-green"
+                ></i>)
+                :
+                (<i 
+                    id="completed"
+                    checked={this.props.todo.completed}
+                    onClick={()=>this.tickTodo(this.props.todo.id)} 
+                    className="list-e-btn-done"
+                ></i>)    
+                
+                }
+                    
+
+                    <i onClick={() =>this.deleteTodo(this.props.todo.id)} className="list-e-btn-del"></i>
                 </div>
             </div>
             
         );
     }
 }
+
+
 
 export default ListElement;
--- src/js/ToDoList.js
+++ src/js/ToDoList.js
@@ -2,8 +2,29 @@ import React, {Component} from 'react'
 import ListElement from './ListElement.js'
 
 function ToDoList(){    
+    //hooks
     const [todos, setTodos] = React.useState([])
     const [todo, setTodo] = React.useState("")
+    const [todoEditing,setTodoEditing] = React.useState(null)
+    const [editingText,setEditingText] = React.useState("")
+
+    
+    React.useEffect(()=>{
+        const temp = localStorage.getItem("todos")
+        const loadedTodos = JSON.parse(temp)
+        if(loadedTodos)
+            setTodos(loadedTodos)
+        },[]
+    )
+    
+    React.useEffect(()=>{
+        const temp = JSON.stringify(todos)
+        localStorage.setItem("todos",temp)
+        },[todos]
+    )
+
+    
+
     const showDate  = new Date().toDateString()
 
     function handleSubmit(e){
@@ -15,7 +36,7 @@ function ToDoList(){
             completed : false
         }
 
-        setTodos([...todos].concat(todo))
+        setTodos([...todos].concat(newTodo))
         setTodo("")
     }
     
@@ -28,7 +49,19 @@ function ToDoList(){
                     <input className="task-input" type="text" placeholder="+ Add task" onChange={(e)=> setTodo(e.target.value)} value={todo}/>
                 </form>
                 <div>
-                    {todos.map((todo)=><ListElement todo={todo} time={showDate}/>)}
+                    {todos.map((todo)=>
+                    <ListElement 
+                        editingText={editingText} 
+                        setTodoEditing={setTodoEditing} 
+                        setEditingText={setEditingText} 
+                        setTodos={setTodos} 
+                        key={todo.id} 
+                        todoEditing={todoEditing} 
+                        todos={todos} 
+                        todo={todo} 
+                        time={showDate}
+                        className="list-e"
+                    />)}
                 </div>
             </div>
         </div>
